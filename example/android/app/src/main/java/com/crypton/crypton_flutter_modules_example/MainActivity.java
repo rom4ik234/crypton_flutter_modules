03e1dd84af80b4b935e420b79448e49bf35e3ebf
Roman <roman@MacBook-Pro-Roman.local>
2021-09-22 13:00:01 +0700

    initial commit

---
+++ example/android/app/src/main/java/com/crypton/crypton_flutter_modules_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.crypton.crypton_flutter_modules_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/CryptonFlutterModulesPlugin.m
@@ -0,0 +1,20 @@
+#import "CryptonFlutterModulesPlugin.h"
+
+@implementation CryptonFlutterModulesPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"crypton_flutter_modules"
+            binaryMessenger:[registrar messenger]];
+  CryptonFlutterModulesPlugin* instance = [[CryptonFlutterModulesPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
